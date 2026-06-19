377ce4c855bb2ec5f76d5f4d0c173dfaab54ab19
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-06-19 04:08:01 -1100

    Update list-menu.js

--- commands/general/list-menu.js
+++ commands/general/list-menu.js
@@ -1,9 +1,11 @@
+cat > commands/general/list-menu.js << 'EOF'
 import { buildList, buildMenu } from '../../utils/helpers.js';
 
 export async function cmdList(sock, msg, from) {
-  await sock.sendMessage(from, { text: buildList() });
+  await sock.sendMessage(from, { text: buildList() }, { quoted: msg });
 }
 
 export async function cmdMenu(sock, msg, from) {
-  await sock.sendMessage(from, { text: buildMenu() });
+  await sock.sendMessage(from, { text: buildMenu() }, { quoted: msg });
 }
+EOF

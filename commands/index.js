3d51d83cdb54eb9370e45182ba943f221addb963
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-06-20 06:23:49 -1100

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,42 +1,29 @@
 // General
-export { cmdPing, cmdHelp }         from './general/ping-help.js';
-export { cmdList, cmdMenu }         from './general/list-menu.js';
-export { cmdProfile }               from './general/profile.js';
-export { cmdSteal }                 from './general/media.js';
+export { cmdPing, cmdHelp }     from './general/ping-help.js';
+export { cmdList, cmdMenu }     from './general/list-menu.js';
+export { cmdProfile }           from './general/profile.js';
+export { cmdSteal }             from './general/media.js';
 
 // Media
-export { cmdViewOnce }              from './media/viewOnce.js';
-export { cmdSend }                  from './media/send.js';
+export { cmdViewOnce }          from './media/viewOnce.js';
+export { cmdSend }              from './media/send.js';
 
 // Group
-export { cmdInfo, cmdTagAll }       from './group/info.js';
-export { cmdKick, cmdPromote,
-         cmdDemote }                from './group/moderation.js';
-export { cmdSubject, cmdLink }      from './group/subject-link.js';
+export { cmdInfo, cmdTagAll }   from './group/info.js';
+export { cmdKick, cmdPromote, cmdDemote } from './group/moderation.js';
+export { cmdSubject, cmdLink }  from './group/subject-link.js';
 
 // Features
-export { cmdAntilink,
-         handleAntilink }           from './antilink.js';
-export { cmdActive, cmdInactive,
-         cmdResetActivity,
-         trackActivity }            from './activity.js';
-export { cmdDk }                    from './domainking.js';
-export { cmdReactions,
-         cmdReactionStats,
-         trackReactions,
-         handleReactionUpdate }     from './reactions.js';
+export { cmdAntilink, handleAntilink }  from './antilink.js';
+export { cmdActive, cmdInactive, cmdResetActivity, trackActivity } from './activity.js';
+export { cmdDk }                from './domainking.js';
+export { cmdReactions, cmdReacted, cmdNotReacted, cmdReactionStats, cmdClearReactions, trackReactions, handleReactionUpdate } from './reactions.js';
 
 // Contacts
-export { cmdContactList,
-         cmdContactSearch,
-         cmdContactSave,
-         cmdContactDelete,
-         cmdContactExport,
-         cmdContactAutoGroup }      from './contacts.js';
+export { cmdContactList, cmdContactSearch, cmdContactSave, cmdContactDelete, cmdContactExport, cmdContactAutoGroup } from './contacts.js';
 
 // Admin
-export { cmdUpdate, cmdReboot }     from './admin/system.js';
-export { cmdBroadcast }             from './admin/broadcast.js';
-export { cmdBankSettings,
-         cmdShowBank }              from './utils/bank.js';
-export { cmdPair }                  from './pair.js';
+export { cmdReboot, cmdUpdate } from './admin/system.js';
+export { cmdBroadcast }         from './admin/broadcast.js';
+export { cmdBank, cmdBnk }      from './utils/bank.js';
+export { cmdPair }              from './pair.js';
